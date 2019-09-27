79671fc3c8e0fabcd26da5c8e698b3c955317b0e
Luca Lorenzini <chielorenz@gmail.com>
2019-09-28 00:25:36 +0200

    UPDATE: cleanup my ideas

--- src/reff-file.js
+++ src/reff-file.js
@@ -3,79 +3,63 @@ const fs = require('fs')
 const fsPromises = require('fs').promises;
 const mimes = require('mime-db');
 const axios = require('axios');
+const forge = require('node-forge');
 
 const url = process.argv[2];
 if(!url) { console.log('Missing url parameter'); return; }
 
-const storageFolder = './storage/';
-const tempFolder = './.tmp/';
-if(!fs.existsSync(storageFolder)) fs.mkdirSync(storageFolder);
-if(!fs.existsSync(tempFolder)) fs.mkdirSync(tempFolder);
-
-console.log('Archiving ' + url);
-
-const origin = (new URL(url)).origin;
-const host = (new URL(url)).host;
+const dir = 'reffs/' + (new URL(url)).host + '/';
+const assets = 'assets/';
+if(!fs.existsSync(dir)) fs.mkdirSync(dir + assets, {recursive: true});
 
 (async () => {
+	console.log('Archiving ' + url);
 	console.log('Loading page');
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto(url);
 	
-	console.log(mimes);
-
 	// Bind puppeteer console to node console
 	page.on('console', obj => console.log(obj.text()));
 
-	console.log('Building hrefs list');
+	console.log('Building external resource list');
 	const resources = await getExternalResources(page);
 
-	console.log('Fetching hrefs', resources);
+	console.log('Fetching external resources');
 	const files = await fetchResources(resources);
 	
-	console.log('Update page references', files);
-	await Promise.all(files.map(async(file) => {
-		let key = file.href !== null ? 'href' : 'src';
-		let value = file[key];
-		let path = file.path;
-
-		await page.evaluate((key, value, path) => {
-			let selectcor = '['+key+'="'+value+'"]';
-			console.log('Updating', selectcor, 'with', key + '=".' + path + '"');
-
-			let elem = document.querySelector(selectcor);
-			elem.setAttribute(key, '.' + path);
-			elem.setAttribute('reff-orig', value);
-			
-			elem.removeAttribute('integrity');
-			elem.removeAttribute('crossorigin');
-		}, key, value, path);
-		return;
-	}));
+	console.log('Updating page references');
+	await updateExternalResources(page, files);
 
 	console.log('Dumping content');
 	const content = await page.content();
 
 	console.log('Building archive');
-	await fsPromises.writeFile(storageFolder + host + '.html', content);
+	await fsPromises.writeFile(dir + '/index.html', content);
 
 	await browser.close();
 	console.log('Done, the archive is ready');
 })();
 
 /**
+ * Get info about external resources of the given page
+ * 
  * @param Page page
- * @return list of hrefs
+ * @return list of node info
  */
-const getExternalResources = (page) => {
+const getExternalResources = page => {
 	return page.$$eval('link,script[src],img', nodes => 
-		nodes.map(node => ({
-			nodeName: node.nodeName,
-			href: node.getAttribute('href'),
-			src: node.getAttribute('src'),
+		nodes.map(node => {
+			const type = node.nodeName === 'LINK' ? 'href' : 'src';
+			console.log('Parsing', node.nodeName, 'with', node.getAttribute(type));
+			return {
+				node: node.nodeName,
+				type: type,
+				attr: node.getAttribute(type),
+				url: node[type],
+			}
 		})
-	));
+	);
 }
 
 /**
@@ -85,68 +69,48 @@ const getExternalResources = (page) => {
 const fetchResources = resources => {
 	return Promise.all(
 		resources.map(async(resource) => {
-			let url = resource.href || resource.src;
-			let isRelative = !url.includes('http');
-			if(isRelative) url = origin + '/' + url;
-
-			const response = await axios.get(url);
-			console.log('axios', url);
-			
-			let contentType = response.headers['content-type'] || 'text/plain';
-			console.log('contentType', contentType);
-
-			if(contentType.includes(';')) contentType = contentType.match(/([^;]+)/g)[0];
-			console.log('contentType', contentType);
-
-			const mime = mimes[contentType];
-			const extention = mime.extensions ? mime.extensions[0] : 'html';
-			console.log('extention', extention);
-
-			const path = tempFolder + getUniq() + '.' + extention;
-			console.log('path', path);
-
-			await fsPromises.writeFile(path, response.data, 'binary');
-			resource.path = path; 
+			const response = await axios.get(resource.url);
+			const headers = response.headers;
+			const type = (headers['content-type'] || 'text/plain').match(/([^;]+)/g)[0];
+			const extention = mimes[type].extensions ? '.' + mimes[type].extensions[0] : '';
+			const name = md5(resource.url) + extention;
+			const savePath = dir + assets + name;
+			console.log('Saving', resource.url, 'to', savePath);
+			await fsPromises.writeFile(savePath, response.data, 'binary');
+			resource.file = name;
+			resource.path = assets + name;
 			return resource;
 		})
 	);
 }
 
 /**
- * Get an unique name from an href
+ * Update the dom elements on the page to use local resources
  * 
- * @param href
- * @return name
+ * @param page
+ * @param files
+ * @return Promise
  */
-const getFileNameFromHref = href => {
-	const unique = Math.random().toString(36).substr(2, 8);
-	const name = href.includes('/') ? href.split('/').slice().pop() : href;
-	return unique + '_' + name;
-}
-
-/**
- * Get unique token 
- */
-const getUniq = () => {
-	return Math.random().toString(36).substr(2, 16);
-}
-
-/**
- * Get the hash of a string
- */
-const hash = (url) => {
-	return require('crypto').createHash('sha1').update(url).digest('base64');
-}
-
-/**
- * Delete a list of file
- * 
- * @param array files path
- */
-const cleanupFiles = files =>{
-	return Promise.all(
-		files.map(async(file) => {
-			await fsPromises.unlink(file.path);
+updateExternalResources = (page, files) => {
+	return new Promise(resolve => 
+		files.forEach(async(file) => {
+			await page.evaluate(file => {
+				let selectcor = '['+file.type+'="'+file.attr+'"]';
+				console.log('Updating', selectcor, 'with', file.type + '=".' + file.path + '"');
+				let elem = document.querySelector(selectcor);
+				elem.setAttribute(file.type, file.path);
+				elem.setAttribute('orig-' + file.type, file.attr);
+				// disable integrity check, why is this needed?
+				elem.removeAttribute('integrity');
+			}, file);
+			resolve(); 
 		})
 	);
+}
+
+/**
+ * Get md5 from url 
+ */
+const md5 = (url) => {
+	return forge.md.md5.create().update(url).digest().toHex();
 }
